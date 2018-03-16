d2ea43bec53f0ad7a5cf8950bd38304b8891e37a
Christian Tietze <christian.tietze@gmail.com>
2018-03-16 08:38:05 +0100

    Initial Commit

---
+++ TagConverter/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  TagConverter
+//
+//  Created by Christian Tietze on 16.03.18.
+//  Copyright © 2018 Christian Tietze. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
